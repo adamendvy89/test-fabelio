02207a21ac2c2f4da1e9cb13ea58a7b4489f67b8
violerine <viosefferine@gmail.com>
2018-11-29 01:13:53 +0700

    db change

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,9 @@ var commentRouter = require ('./routes/comments')
 
 var app = express();
 app.use(cors())
-mongoose.connect("mongodb://127.0.0.1:27017/webScraping",{useNewUrlParser:true})
+
+// mongoose.connect("mongodb://127.0.0.1:27017/webScraping",{useNewUrlParser:true})
+mongoose.connect("mongodb://testfabelio:testfabelio@cluster0-shard-00-00-agake.gcp.mongodb.net:27017,cluster0-shard-00-01-agake.gcp.mongodb.net:27017,cluster0-shard-00-02-agake.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",{useNewUrlParser:true})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
