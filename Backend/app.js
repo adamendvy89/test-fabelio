d16cf5d42d480fb463ffe1701457bd99e219bfea
violerine <viosefferine@gmail.com>
2018-11-29 21:36:03 +0700

    try heroku

--- Backend/app.js
+++ Backend/app.js
@@ -17,8 +17,8 @@ var cronRouter = require ('./routes/cronjob')
 var app = express();
 app.use(cors())
 
-mongoose.connect("mongodb://127.0.0.1:27017/webScraping",{useNewUrlParser:true})
-
+// mongoose.connect("mongodb://127.0.0.1:27017/webScraping",{useNewUrlParser:true})
+mongoose.connect("mongodb://testfabelio:testfabelio@cluster0-shard-00-00-agake.gcp.mongodb.net:27017,cluster0-shard-00-01-agake.gcp.mongodb.net:27017,cluster0-shard-00-02-agake.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",{useNewUrlParser:true})
 
 // view engine setup
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
